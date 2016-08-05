7db78b44a27855557277ec4d475da02ec4a4c213
Zazz <captain.zazz@gmail.com>
2016-08-06 00:12:17 +0100

    Add dependency checking to web-full.

--- entry/web-full.js
+++ entry/web-full.js
@@ -8,4 +8,12 @@ adapters.JSONPRequestAdapter   = require('../src/api/request/adapter/JSONPReques
 adapters.XMLHttpRequestAdapter = require('../src/api/request/adapter/XMLHttpRequestAdapter').default;
 library.API.APIService.setAdapter(adapters.JSONPRequestAdapter);
 
+try{
+    if (typeof Promise === 'undefined') throw new ReferenceError("No Promise found");
+}catch(e){
+    var errorMessage = "Error checking for dependency. " + e.message + ". A polyfill for this platform may be needed.";
+    if (typeof alert !== 'undefined') alert(errorMessage);
+    console.error(errorMessage);
+}
+
 module.exports = library;
