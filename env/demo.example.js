94679504bcfcc32a3f16410a0be7ad2d389e5485
Zazz <captain.zazz@gmail.com>
2016-08-28 14:57:49 +0100

    Pre-populate Auth demo page based on env/demo.js

--- env/demo.example.js
+++ env/demo.example.js
@@ -1,10 +1,16 @@
 /**
  * Settings used to run demos
- * @type {{PLAYER_USERNAME: string, PLAYER_PASSWORD: string, PLAYER_BASE_URL: string, PLAYER_REALTIME_URL: string}}
+ * @type {{PLAYER_USERNAME:string, PLAYER_PASSWORD:string, PLAYER_BASE_URL:string, PLAYER_REALTIME_URL:string, OAUTH_CLIENT_ID:string, OAUTH_CLIENT_SECRET:string}}
  */
-module.exports = {
- // PLAYER_USERNAME: '<username>',
- // PLAYER_PASSWORD: '<password>',
-    PLAYER_BASE_URL: 'https://player.me',
-    PLAYER_REALTIME_URL: 'https://player.me:443'
+var obj = {
+ // PLAYER_USERNAME:     '<username>',
+ // PLAYER_PASSWORD:     '<password>',
+    PLAYER_BASE_URL:     'https://player.me',
+    PLAYER_REALTIME_URL: 'https://player.me:443',
+//  OAUTH_CLIENT_ID:     '<client_id>',
+//  OAUTH_CLIENT_SECRET: '<client_secret>'
 };
+
+// Export
+if (typeof module == 'object') module.exports = obj;
+if (typeof define == 'function') define(obj);
